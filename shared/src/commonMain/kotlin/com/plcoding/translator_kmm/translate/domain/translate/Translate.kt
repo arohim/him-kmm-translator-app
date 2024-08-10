16f86506ce1c6f5ad6e899ad424e68670c0dcee8
Abdulrohim 'Him' Sama <abdulrohimhim.s@its-MacBook-Pro.local>
2024-08-10 21:43:23 +0700

    Feat: Added Translate use case

---
+++ shared/src/commonMain/kotlin/com/plcoding/translator_kmm/translate/domain/translate/Translate.kt
@@ -0,0 +1,34 @@
+package com.plcoding.translator_kmm.translate.domain.translate
+
+import com.plcoding.translator_kmm.core.domain.history.HistoryDataSource
+import com.plcoding.translator_kmm.core.domain.history.HistoryItem
+import com.plcoding.translator_kmm.core.domain.langauge.Language
+import com.plcoding.translator_kmm.core.domain.util.Resource
+
+class Translate(
+    private val client: TranslateClient,
+    private val historyDataSource: HistoryDataSource
+) {
+    suspend fun execute(
+        fromLanguage: Language,
+        fromText: String,
+        toLanguage: Language
+    ): Resource<String> {
+        return try {
+            val translatedText = client.translate(fromLanguage, fromText, toLanguage)
+            historyDataSource.insertHistoryItem(
+                item = HistoryItem(
+                    id = null,
+                    fromText = fromText,
+                    toText = translatedText,
+                    fromLanguageCode = fromLanguage.langCode,
+                    toLanguageCode = toLanguage.langCode
+                )
+            )
+            Resource.Success(translatedText)
+        } catch (e: TransactionException) {
+            e.printStackTrace()
+            Resource.Error(e)
+        }
+    }
+}
